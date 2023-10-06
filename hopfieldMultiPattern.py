7705dae0610983a8bfa7893a5c5bcf638d0ce1ad
Bartłomiej Domański <135234591+kebabee@users.noreply.github.com>
2023-10-06 16:13:39 +0200

    Update hopfieldMultiPattern.py

--- hopfieldMultiPattern.py
+++ hopfieldMultiPattern.py
@@ -11,13 +11,13 @@ class hopfieldNet:
         self.patterns = patterns  # Przypisz listę wzorców do atrybutu patterns
         num_patterns = len(patterns)
     
-        # Zresetuj macierz wag
+        # wyzerowanie macierzy wag
         self.weights = np.zeros((self.numOfNeurons, self.numOfNeurons))
     
         for i in range(self.numOfNeurons):
             for j in range(self.numOfNeurons):
                 if i != j:
-                    for pattern in patterns:
+                    for pattern in patterns: #uczenie dla wszystkich wzorców
                         self.weights[i][j] += pattern[i] * pattern[j]
                     self.weights[i][j] /= (self.numOfNeurons * num_patterns) #normalizujemy przez liczbę neuronów
                     #if self.weights[i][j] > 0: #normalizujemy do 1 lub -1
@@ -35,7 +35,7 @@ class hopfieldNet:
             newData = np.where(newData >= 0, 1, -1) #aktualizacja stanu zgodnie z funkcją aktywacji
             print(f"stan po:    {newData}")
             data = newData #nadpisywanie danych
-            #jeżeli dane są identyczne jak wzorzece albo przeciwności wzorca to kończymy działanie
+            #jeżeli dane są identyczne jak wzorce albo przeciwności wzorca to kończymy działanie
             for pattern in self.patterns:
                 if np.array_equal(data, pattern) or np.array_equal(data, -pattern):
                     return data
@@ -61,8 +61,8 @@ testData1 = np.random.choice([-1, 1], size=40)
 testData2 = np.random.choice([-1, 1], size=40)
 testData3 = np.random.choice([-1, 1], size=40)
 print("\nTest1")
-result1 = network.test(testData1, 50) #poprawnie odtworzony wzorzec
+result1 = network.test(testData1, 50)
 print("\nTest2")
-result2 = network.test(testData2, 50) #poprawnie odtworzony wzorzec
+result2 = network.test(testData2, 50)
 print("\nTest3")
-result3 = network.test(testData3, 50) #poprawnie odtworzony wzorzec
+result3 = network.test(testData3, 50)
